32a8cb5f47f245f74361bbd9179089a0b1f47252
saiyamkumar <33348125+saiyamkumar@users.noreply.github.com>
2017-11-22 11:24:00 +0100

    Add files via upload

---
+++ esCiclo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+// ESERCIZIO COMPITO Saiyam Kumar 5BIA
+void main ()
+<<<<<<< HEAD
+{	
+	
+	// parametro intero contatore 
+	
+=======
+{   
+>>>>>>> FIX_BUG
+	int contatore = 0;
+	// Il for fa un ciclo fino a dieci e stampa il valore 
+	for (int i= 0;i < 10;i++) 
+	{
+		contatore++;
+		cout << contatore <<endl;
+	}	
+<<<<<<< HEAD
+	//esercizio fatto
+=======
+>>>>>>> FIX_BUG
+}
